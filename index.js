7d628ca23450473b4bef36b80ff07eadcb9b4283
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-01-15 23:16:18 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -14,23 +14,26 @@ var Discord = new eris.CommandClient(Tokens.DiscordKey, {}, {
     "prefix": "!"
 });
 
+Discord.on('ready',()=>{
+    console.log("Ready!");
+})
 //var sqlite3 = require('sqlite3');
 //var sqlcon = sqlite3.Database('Profiles.db');
 
 
 //UserProfile Command.
 Discord.registerCommand("user", (msg, args) => {
-    DiscordOsu.FetchUser(Osu,Discord,msg,args);
+    DiscordOsu.FetchUser(Osu, Discord, msg, args);
     //Test.FetchUser(Osu, Discord, msg, args);
 }, {
         "description": "Fetches User Profile.",
         "usage": "!user <Username> [Usernames...]"
     });
-
 var useraliases = ['u', 'usr', 'profile', 'osu']
-useraliases.forEach(function(Alias) {
-    Discord.registerCommandAlias(Alias,"user");
+useraliases.forEach(function (Alias) {
+    Discord.registerCommandAlias(Alias, "user");
 });
+
 
 
 Discord.registerCommand("recent",(msg,args) => {
