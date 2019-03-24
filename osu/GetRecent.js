f7783c2a1c728bad43e6360508c424f96ddbb511
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-03-24 12:57:39 +0530

    Formatting Changes

--- osu/GetRecent.js
+++ osu/GetRecent.js
@@ -1,37 +1,38 @@
 var embed = require('../embedCreator');
 var util = require('./utils');
 
-module.exports.GetRecent = async function GetRecent(Osu, Discord, msg, msgargs,db) {
-    if (msgargs.length == 0) {
-        var result;
-        await db.get("SELECT OsuUsername from USERS WHERE userID = ?", msg.author.id, async (err, row) => {
-            if (!err)
-                result = await GetUserScores(Osu, row.OsuUsername)
-            else
-                result = "Please Enter a User"
-            Discord.createMessage(msg.channel.id, result);
-        });
-    }
-    else {
-        msgargs.forEach(async (user) => {
-            try {
-                var em = await GetUserScores(Osu, user);
-                Discord.createMessage(msg.channel.id, em)
-            }
-            catch(err)
-            {
-                console.log(err)
-                Discord.createMessage(msg.channel.id,"Please Specify the correct ID.");
-            }
-        });
-    }
+module.exports.GetRecent = async function GetRecent(Osu, Discord, msg, msgargs, db) {
+	//To Get the recent scores of the given User.
+	var res;
+	if (msgargs.length == 0) {
+		//What happens when there's no username specified.
+		//Searching for username
+		db.find({
+			discordID: msg.author.id
+		}, (err, docs) => {
+			//if found, then docs is not empty
+			if (docs.length == 0) {
+				res = new embed("Recent Scores", "You were not found in the database, set your username.");
+			} else {
+				res = await GetUserScores(Osu, docs[0].OsuID);
+			}
+		})
+	} else {
+		var username = msgargs[0];
+	}
+	Discord.sendMessage(msg.channel.id, res.construct())
 }
 
 module.exports.userScores = async function GetUserScores(Osu, user) {
-    var Scores = await Osu.getUserRecent({u: user,limit: 1});
-    var Beatmapinfo = await Osu.getBeatmaps({b:Scores[0].beatmapId});
-    var desc = `${Beatmapinfo[0].title}+[${Beatmapinfo[0].version}]${util.ParseDiff(Scores[0].mods)}\n`;
-    desc+= `${Scores[0].maxCombo}/${Beatmapinfo[0].maxCombo} | Acc: ${util.ParseAcc(Scores[0].counts)}%`
-    var em = new embed("Recent Score for"+user,desc)
-    return em;
+	var Scores = await Osu.getUserRecent({
+		u: user,
+		limit: 1
+	});
+	var Beatmapinfo = await Osu.getBeatmaps({
+		b: Scores[0].beatmapId
+	});
+	var desc = `${Beatmapinfo[0].title}+[${Beatmapinfo[0].version}]${util.ParseDiff(Scores[0].mods)}\n`;
+	desc += `${Scores[0].maxCombo}/${Beatmapinfo[0].maxCombo} | Acc: ${util.ParseAcc(Scores[0].counts)}%`
+	var em = new embed("Recent Score for" + user, desc)
+	return em;
 }
