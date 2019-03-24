c071bdd0a6167431d38e411378285e6cff266295
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-03-24 17:43:37 +0530

    Roadmap Updated and SetUser works with nedb.

---
+++ Roadmap.md
@@ -0,0 +1,5 @@
+# Things to do
+
+1. Implement the tracking api that fetches the data from the api.
+2. Format the text properly.
+3. Beatmap recognition with scores.
--- osu/setUser.js
+++ osu/setUser.js
@@ -1,22 +1,37 @@
 const embed = require("../embedCreator");
+
+
 module.exports = async function setUser(Osu, Discord, msg, msgargs, db) {
-	var sendstr;
-	var userID = msg.author.id;
-	var osuUserName = msgargs[0];
+	var sendstr = new embed();
+	var osuUserName = msgargs[0]; //msgargs are seperated by ' ' so 0th arg should be name.
+
 	if (msgargs.length === 0) {
-		sendstr = new embed("Username error", "Enter a name for setting.");
+		//Username to set not Specified.
+		sendstr.withTitle("Username Confirmation")
+		sendstr.withDesc("You have not set a user to be tracked.");
 	}
+
 	db.find({
 		user: osuUserName
 	}, (err, docs) => {
 		if (docs.length > 0) {
-			sendstr = new embed("Username Confirmation", "Name has been set.")
+			//Name has been set already. 
+			//TODO:
+			//Make the command remap to a new user if specified again.
+			sendstr.withTitle("Username Confirmation");
+			sendstr.withDesc("Name has already been set.");
 		} else {
-			sendstr = "Nameset!";
+			//TODO:
+			/**Add additional feilds when inserting the player information.
+			 * Implement secondary DB data fetch (One for the Player Data so that it can be stored locally)(needed?);
+			 */
+			sendstr.withTitle("Username Confirmation");
+			sendstr.withDesc(`Your username has been set as ${osuUserName}`);
+
 			db.insert({
 				discordID: msg.author.id,
 				OsuID: osuUserName
-			})
+			}); //DB now has been inserted to. ERROR check is not done. TODO.
 		}
 	});
 	Discord.createMessage(msg.channel.id, sendstr);
