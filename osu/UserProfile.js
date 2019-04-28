9bec280dae8d12cce022c24fc6175c8347ecf025
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-04-28 12:42:10 +0530

    Cleanup of Userprofile.

--- osu/UserProfile.js
+++ osu/UserProfile.js
@@ -1,47 +1,48 @@
-import condense from "./utils"
+var utils = require('./utils')
 
 const embed = require('../embedCreator');
 module.exports = async function GetUserScores(Osu, Discord, msg, msgargs, db) {
-	if (msgargs.length == 0) {
-		db.find({
-			DiscordId: msg.author.id
-		}, async function (err, docs) {
-			if (!err && docs) {
-				var result = await GetUserData(Osu, docs.username);
-			}
-			Discord.createMessage(msg.channel.id, result);
-		})
-	} else {
-		msgargs.forEach(async function (user) {
-			try {
-				var em = await GetUserData(Osu, user);
-				Discord.createMessage(msg.channel.id, em);
-			} catch (err) {
-				console.log(err);
-				Discord.createMessage(msg.channel.id, "User Doesn't exist!");
-			}
-		});
-	}
+    if (msgargs.length == 0) {
+        db.find({
+            discordID: msg.author.id
+        }, function(err, docs) {
+            if (!err || !docs)
+                procUser(Osu, Discord, docs[0].OsuID, msg.channel.id);
+            else
+                Discord.createMessage(msg.channel.id, "Havent Set your ID!")
+        })
+    } else {
+        procUser(Osu, Discord, msgargs[0], msg.channel.id);
+    };
+}
+
+async function procUser(Osu, Discord, username, channelID) {
+    try {
+        var em = await GetUserData(Osu, username);
+        Discord.createMessage(channelID, em);
+    } catch (err) {
+        Discord.createMessage(channelID, `Something went wrong: ${err}`)
+    }
 }
 
 async function GetUserData(Osu, user) {
-	let Info = await Osu.getUser({
-		u: user
-	});
+    let Info = await Osu.getUser({
+        u: user
+    });
 
-	/*Constructing the Description*/
-	var desc = condense("\n",
-		`Rank:${Info.pp.rank}`,
-		`**PP:${Info.pp.raw}*`,
-		`Level:${Info.level}`,
-		`Country:${Info.country}`,
-		`CountryRank:${Info.pp.countryRank}`,
-		`PlayCount:${Info.counts.plays}`,
-		`Accuracy:${Info.accuracyFormatted}`)
+    /*Constructing the Description*/
+    var desc = utils.condense("\n",
+        `Rank:${Info.pp.rank}`,
+        `**PP:${Info.pp.raw}**`,
+        `Level:${Info.level}`,
+        `Country:${Info.country}`,
+        `CountryRank:${Info.pp.countryRank}`,
+        `PlayCount:${Info.counts.plays}`,
+        `Accuracy:${Info.accuracyFormatted}`)
 
-	var em = new embed(`Here's what you *probably* need...?`, desc);
+    var em = new embed(`Here's what you *probably* need...?`, desc);
 
-	em.withAuthor(user, `https://osu.ppy.sh/images/flags/${Info.country}.png`);
-	em.Withthumb(`https://a.ppy.sh/${Info.id}`);
-	return em;
+    em.withAuthor(user, `https://osu.ppy.sh/images/flags/${Info.country}.png`);
+    em.Withthumb(`https://a.ppy.sh/${Info.id}`);
+    return em;
 }
