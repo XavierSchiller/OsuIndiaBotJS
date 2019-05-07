64c7c207e948b6ffa8dea8d2e94785841e0e6be6
Sabarna Chakravarty <sabarnac@gmail.com>
2019-05-07 18:36:21 +0530

    Untrack Build Artifacts

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare var Tokens: any;
-declare var nodeosu: any;
-declare var ModL: any;
-declare var eris: any;
-declare var CmdConf: any;
-declare var nedb: any, db: any;
-declare var Mods: any;
-declare var Osu: any;
-declare var Discord: any;
