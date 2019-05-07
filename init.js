9c272ae155be5a36dd840c53b211e518dd029dee
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-05-07 13:14:01 +0530

    Initalize Script

--- init.js
+++ init.js
@@ -1,6 +1,8 @@
-var Discord = require("./discord")
-var fs = require('fs')
+var Discord = require("./discord");
+var fs = require('fs');
+
 module.exports = function init() {
+    initCommands();
     initEvents();
     Discord.connect().catch(err => {
         console.log(err)
@@ -11,5 +13,17 @@ function initEvents() {
     var events = ["messageCreate"];
     events.forEach(event => {
         Discord.on(event, require("./events/" + event));
+    })
+}
+
+
+function initCommands() {
+    //get Osu Commands;
+    var x = require('./osuCommands')
+    Discord.registerCommand("getuser", (message, args) => {
+        x.getUser(message, args).then(em => {
+            Discord.createMessage(message.channel.id, em)
+        })
+
     })
 }
