010138e16535492b3c9a41a6759c91d912ab5a70
Sabarna Chakravarty <sabarnac@gmail.com>
2019-05-07 19:45:54 +0530

    Update Webpack Configuration, Fix Formatting

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,36 +1,43 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const path = require("path")
+const nodeExternals = require("webpack-node-externals")
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 
 module.exports = {
-    target: 'node',
-    mode: 'development',
-    entry: './src/index',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js',
-        libraryTarget: 'commonjs2'
-    },
-    externals: [nodeExternals()],
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-        rules: [{
-            // Include ts, tsx, js, and jsx files.
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['env', {
-                            targets: {
-                                node: 'current'
-                            },
-                        }],
-                    ]
-                }
-            }
-        }],
-    }
-};
+  target: "node",
+  mode: "development",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "app.bundle.js",
+    libraryTarget: "commonjs2",
+  },
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        // Include ts, tsx, js, and jsx files.
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "env",
+                {
+                  targets: {
+                    node: "current",
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+}
