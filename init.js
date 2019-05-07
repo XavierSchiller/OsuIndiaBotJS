f08872f97ce934a92fc67f8dc8d468b309fccd13
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-05-07 14:00:45 +0530

    Changes to init

--- init.js
+++ init.js
@@ -24,6 +24,50 @@ function initCommands() {
         x.getUser(message, args).then(em => {
             Discord.createMessage(message.channel.id, em)
         })
+    }, {
+        aliases: ["osu", "user", "profile"],
+        caseInsensitive: false,
+        cooldown: 5,
+        cooldownMessage: "Wait for 5 seconds before using this command again!",
+        usage: "top username"
+    })
 
+
+    Discord.registerCommand("getUserBest", (message, args) => {
+        x.getUserBest(message, args).then(em => {
+            Discord.createMessage(message.channel.id, em)
+        })
+    }, {
+        aliases: ["osu", "user", "profile"],
+        caseInsensitive: false,
+        cooldown: 5,
+        cooldownMessage: "Wait for 5 seconds before using this command again!",
+        usage: "top username"
+    })
+
+
+    Discord.registerCommand("getRecent", (message, args) => {
+        x.getRecent(message, args).then(em => {
+            Discord.createMessage(message.channel.id, em)
+        })
+    }, {
+        aliases: ["osu", "user", "profile"],
+        caseInsensitive: false,
+        cooldown: 5,
+        cooldownMessage: "Wait for 5 seconds before using this command again!",
+        usage: "top username"
+    })
+
+
+    Discord.registerCommand("setUser", (message, args) => {
+        x.setUser(message, args).then(em => {
+            Discord.createMessage(message.channel.id, em)
+        })
+    }, {
+        aliases: ["osu", "user", "profile"],
+        caseInsensitive: false,
+        cooldown: 5,
+        cooldownMessage: "Wait for 5 seconds before using this command again!",
+        usage: "top username"
     })
 }
--- osuCommands/osu.js
+++ osuCommands/osu.js
@@ -1,10 +1,10 @@
-var Tokens = require("./config.json");
+var Tokens = require("../config.json");
 
 var nodeosu = require("node-osu");
 
-var Osu = new nodeosu.Api(Tokens.OsuApiKey, {
+var osu = new nodeosu.Api(Tokens.OsuApiKey, {
     notFoundAsError: true,
     completeScores: false
 });
 
-export default Osu;
+module.exports = osu;
