23e1dd55d4b4ebe54ac8c0be43ae0cdcc8e95caa
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-05-07 22:29:27 +0530

    Formatting fixes by ESLint.

--- osuCommands/GetRecent.js
+++ osuCommands/GetRecent.js
@@ -1,36 +1,56 @@
-var embed = require('../embedCreator');
-var util = require('./utils');
+const embed = require('../utils/embedCreator');
+const util = require('./utils');
+const db = require('../configs/nedb');
+const Osu = require('../configs/osu');
+module.exports = function GetRecent(msg, msgargs) {
+	let em;
+	if (msgargs.length == 0) {
+		em = db.find(
+			{
+				discordID: msg.author.id,
+			},
+			(err, docs) => {
+				debugger;
+				if (!err || !docs) {
+					return getUserScores(docs[0].OsuID);
+				} else {
+					return new embed(
+						'ID Has not been set',
+						'Set your ID to utilize this command!'
+					);
+				}
+			}
+		);
+	} else {
+		em = getUserScores(msgargs[0]);
+	}
+	console.log(em);
+	return em;
+};
 
-module.exports = async function GetRecent(Osu, Discord, msg, msgargs, db) {
-    if (msgargs.length == 0) {
-        db.find({
-            discordID: msg.author.id
-        }, function(err, docs) {
-            if (!err || !docs)
-                utils.procUser(Osu, Discord, username, channelID, GetUserScores)
-            else
-                Discord.createMessage(msg.channel.id, "Havent Set your ID!")
-        })
-    } else {
-        utils.procUser(Osu, Discord, username, channelID, GetUserScores)
-    };
-}
-
-async function GetUserScores(Osu, user) {
-    console.log(user);
-    try {
-        var Scores = await Osu.getUserRecent({
-            u: user,
-            limit: 1
-        });
-        var Beatmapinfo = await Osu.getBeatmaps({
-            b: Scores[0].beatmapId
-        });
-    } catch (err) {
-        return new embed("Something Went Wrong", `Error Description:${err}`);
-    }
-    var desc = `${Beatmapinfo[0].title}+[${Beatmapinfo[0].version}]${util.ParseDiff(Scores[0].mods)}\n`;
-    desc += `${Scores[0].maxCombo}/${Beatmapinfo[0].maxCombo} | Acc: ${util.ParseAcc(Scores[0].counts)}%`
-    var em = new embed("Recent Score for" + user, desc)
-    return em;
+/**
+ * Fetches a single score from the osuapi and returns a promise.
+ * @param {string} user
+ */
+async function getUserScores(user) {
+	console.log(user);
+	try {
+		const Scores = await Osu.getUserRecent({
+			u: user,
+			limit: 1,
+		});
+		const Beatmapinfo = await Osu.getBeatmaps({
+			b: Scores[0].beatmapId,
+		});
+	} catch (err) {
+		return new embed('Something Went Wrong', `Error Description:${err}`);
+	}
+	let desc = `${Beatmapinfo[0].title}+[${
+		Beatmapinfo[0].version
+	}]${util.ParseDiff(Scores[0].mods)}\n`;
+	desc += `${Scores[0].maxCombo}/${
+		Beatmapinfo[0].maxCombo
+	} | Acc: ${util.ParseAcc(Scores[0].counts)}%`;
+	const em = new embed('Recent Score for' + user, desc);
+	return em;
 }
