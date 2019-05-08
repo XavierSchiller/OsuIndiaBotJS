99bc1d2646b4a7c6a7baf4870ce1b79cab8dc484
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-05-08 11:13:33 +0530

    Discord Wrapper Functions.

--- src/Clients/discord.js
+++ src/Clients/discord.js
@@ -1,4 +1,4 @@
-const Tokens = require('../config/config.json');
+const Tokens = require('../configs/config.json');
 const eris = require('eris');
 
 const Discord = new eris.CommandClient(
@@ -13,4 +13,57 @@ const Discord = new eris.CommandClient(
     }
 );
 
-module.exports = Discord;
+/**
+ * Invokes registerCommand() within eris with additional functionality.
+ * @param {string} commandName Name of the command that needs to be registered.
+ * @param {function} functionName Callback to the listener.
+ * @param {Array<string>} aliases String array of aliases.
+ */
+function addCommand(commandName, functionName, aliases) {
+  Discord.registerCommand(
+      commandName,
+      (msg, args) => {
+        functionName(msg, args);
+      },
+      {
+        aliases: aliases,
+        caseInsensitive: false,
+        cooldown: 5,
+        cooldownMessage: 'Wait for 5 seconds before using this command again!',
+      }
+  );
+}
+
+/**
+ *
+ * @param {embed} data Embed data you wanted to send.
+ * @param {string} channelID Specified Channel.
+ */
+function sendMessage(data, channelID) {
+  Discord.createMessage(channelID, data).catch((err) => {
+    console.log(err);
+  });
+}
+
+/**
+ *
+ * @param {event} eventName The Name of the event.
+ * @param {function} functionName Callback for the event.
+ */
+function onEvent(eventName, functionName) {
+  Discord.on(eventName, (call, oldcall) => {
+    functionName(call, oldcall);
+  });
+}
+/**
+ * Starts the Discord Client.
+ */
+function start() {
+  Discord.connect().catch((err) => console.log(err));
+}
+module.exports = {
+  addCommand: addCommand,
+  sendMessage: sendMessage,
+  onEvent: onEvent,
+  start: start,
+};
